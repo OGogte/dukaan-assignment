b69c6b5fa07a1e7cd6bf93a8820812e3bbd29a90
OGogte <omg.omgogte@gmail.com>
2024-01-09 03:04:08 +0530

    Header done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import styles from '../styles/Header.module.css';
 import question from '../assets/question.svg';
-import search from '../assets/search.svg'
+import search from '../assets/search.svg';
+import ellipse from '../assets/ellipse.svg';
+import messages from '../assets/messages.svg';
+import headerDown from '../assets/headerDown.svg';
 export default function Header() {
     return (
         <div className={styles.header}>
-
+            <div className={styles.leftTextBox}>
+                <div className={styles.title}> Payments</div>
+                <div className={styles.info}>
+                    <img className={styles.infoIcon} src={question} alt='question' />
+                    <div className={styles.infoText}>How it works
+                    </div>
+                </div>
+            </div>
+            <div className={styles.searchBarBox}>
+                <img className={styles.searchBarIcon} src={search} alt='Search' />
+                <div className={styles.searchBarText} >Search features, tutorials, etc.</div>
+            </div>
+            <div className={styles.iconBox}>
+                <img src={messages} alt='messages' className={styles.icon} />
+                <img src={headerDown} alt='down arrow' className={styles.icon} />
+            </div>
         </div>
     )
 }
--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Navbar from './Components/Navbar';
 import Header from './Components/Header';
 import styles from './styles/Dashboard.module.css'
+import Table from './Components/Table';
 export default function Dashboard() {
   return (
     <div className={styles.dashboard}>
       <div>
         <Navbar />
       </div>
-
+      <div>
+        <Header />
+        <Table />
+      </div>
     </div>
   )
 }
